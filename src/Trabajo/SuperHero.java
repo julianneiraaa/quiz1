fd02f9811be63a06b3f5f018555e7a4667321f93
julianneiraaa <161834207+julianneiraaa@users.noreply.github.com>
2024-04-05 21:03:04 -0500

    SuperHero.java

--- src/Trabajo/SuperHero.java
+++ src/Trabajo/SuperHero.java
@@ -2,52 +2,14 @@ package Trabajo;
 
 import java.util.Scanner;
 
-public class SuperHero 
-{
-	
-	public static void main(String[] args) 
-	{
+public class Superhero extends persona {
+
+	public Superhero(persona person, String heroname, String) {
+		this.name= person.name;
+		this.edad= person
 		
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Ingrese el nombre de la persona:");
-        String nombrePersona = scanner.nextLine();
-        System.out.println("Ingrese la edad de la persona:");
-        int edadPersona = scanner.nextInt();
-        
-        Persona persona = new Persona(nombrePersona, edadPersona);
-        
-        System.out.println("\nInformacion de la persona:");
-        persona.MostrarInformacion();
-        
-        scanner.nextLine(); 
-        
-        System.out.println("\nIngrese el nombre del empleado:");
-        String nombreEmpleado = scanner.nextLine();
-        System.out.println("Ingrese la edad del empleado:");
-        int edadEmpleado = scanner.nextInt();
-        scanner.nextLine(); 
-        System.out.println("Ingrese el cargo del empleado:");
-        String cargoEmpleado = scanner.nextLine();
-        System.out.println("Ingrese el salario del empleado:");
-        double salarioEmpleado = scanner.nextDouble();
-        
-        Main main = new Main(nombreEmpleado, edadEmpleado, salarioEmpleado, cargoEmpleado);
-        
-        System.out.println("\nInformacion del empleado:");
-        main.MostrarInformacion();
-        
-        
-        System.out.println("\nPreguntas QUIZ:");
-        persona.responderPreguntas();
-        
-        
-        
-        
-        
-        
-       
-        scanner.close();
-    }
+	}
+	String herohame;
+	String powers[];
 
 }
