1f621767d60abad6cc92247f4b8c6e7e1c5140ae
julianneiraaa <161834207+julianneiraaa@users.noreply.github.com>
2024-04-05 21:08:48 -0500

    Update Main.java

--- src/Trabajo/Main.java
+++ src/Trabajo/Main.java
@@ -1,24 +1,22 @@
 package Trabajo;
 
-public class Main extends Persona
+public class Main
 {
-	private Double salario;
-	private String cargo;
-	
-	
-	
-	public Main(String nombre, int edad, Double salario, String cargo) 
-	{
-		super(nombre, edad);
-		this.salario = salario;
-		this.cargo = cargo;
-	}
-	
 
-	 public void MostrarInformacion()
-	 {
-	        super.MostrarInformacion(); 
-	        System.out.println("Cargo: " + cargo);
-	        System.out.println("Salario: " + salario);
-	    }
+	public static void main(String[] args) 
+	{
+		PersonaEFS tony_stark = new PersonaEFS("Tony Stark", 35);
+		
+		System.out.println(tony_stark.nombre + ""+tony_stark.edad);
+	
+		tony_stark.setEdad(35);
+		
+		System.out.println(tony_stark.nombre + ""+tony_stark.edad);
+		
+		SuperHeroEFS iron_man = new SuperHeroEFS(tony_stark.nombre, tony_stark.edad);
+				
+		iron_man.supernombre = "Iron Man";
+		iron_man.poderes = new String[] {"Volar", "Pelear"};
+	}
+
 }
